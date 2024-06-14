8fafabcb46812ac908b7e873bcc74810b86917ec
C:\Users\Alejandro Sanchez\Desktop\REPOSITORIOS <alsasou8@gmail.com>
2024-06-14 16:10:18 +0200

    Pokemon hasta Cuarta Gen + Adicciones en la historia

--- Plugins/Level Caps EX/000_Config.rb
+++ Plugins/Level Caps EX/000_Config.rb
@@ -7,11 +7,11 @@ module LevelCapsEX
 
   # Set this to the Game Variable which controls the mode of the
   # level cap. The 3 modes are:
-  #   0 (Hard Cap) - The Pokemon cannot level up/gain experience 
+  #   1 (Hard Cap) - The Pokemon cannot level up/gain experience 
   #   past the level cap.
-  #   1 (EXP Cap) - The Pokemon will gain reduced EXP when it
+  #   2 (EXP Cap) - The Pokemon will gain reduced EXP when it
   #   crosses the level cap.
-  #   2 (Obedience Cap)  - The Pokemon will gain disobey the player
+  #   3 (Obedience Cap)  - The Pokemon will gain disobey the player
   #   when it crosses the level cap.
   LEVEL_CAP_MODE_VARIABLE = 77
 
